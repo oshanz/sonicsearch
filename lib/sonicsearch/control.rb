95f64ac7b727e4c2484397b39a61419cc5f25c3e
Oshan Wisumperuma <warunaoshan@gmail.com>
2019-03-29 01:47:55 +0530

    async control

--- lib/sonicsearch/control.rb
+++ lib/sonicsearch/control.rb
@@ -1,7 +1,18 @@
+# frozen_string_literal: true
+
 module Sonicsearch
   class Control
-    def trigger(action); end
+    def connection
+      ChannelFactory.new.control_channel
+    end
 
-    def close; end
+    def trigger(action)
+      tcp_client = connection.connect do |peer|
+        peer.write("TRIGGER #{action}")
+        message = peer.read(512)
+
+        puts "Sent #{data}, got response: #{message}"
+      end
+    end
   end
 end
