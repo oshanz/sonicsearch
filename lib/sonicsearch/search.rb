476aaa6de41a3d800c398b1409c4c3d4a0d98940
Oshan Wisumperuma <warunaoshan@gmail.com>
2019-03-29 01:47:36 +0530

    async query

--- lib/sonicsearch/search.rb
+++ lib/sonicsearch/search.rb
@@ -1,13 +1,53 @@
-## connection create
+# frozen_string_literal: true
 
-##  query
-
-## close
+require 'async/io'
 
 module Sonicsearch
   class Search
-    def query(ollection_id, bucket_id, terms_text, done_cb, limit, offset); end
+    def connection
+      ChannelFactory.new.search_channel
+    end
 
-    def suggest(ollection_id, bucket_id, word_text, done_cb, limit, offset); end
+    def query(collection_id, bucket_id, terms_text, limit, offset)
+      cmd = "QUERY #{collection_id} #{bucket_id} #{terms_text}"
+      cmd += " LIMIT(#{limit})" unless limit.nil?
+      cmd += " OFFSET(#{limit})" unless offset.nil?
+      connection.connect do |peer|
+        peer.write(cmd)
+        message = peer.read(512) # PENDING CjPvE5t9
+        puts "Sent #{data}, got response: #{message}"
+      end
+      search_done = false
+      while search_done
+        Async do |_task|
+          endpoint.connect do |peer|
+            message = peer.read(512) # received EVENT QUERY CjPvE5t9
+            puts "Sent #{data}, got response: #{message}"
+            search_done = true
+          end
+        end
+      end
+    end
+
+    def suggest(collection_id, bucket_id, word_text, limit, offset)
+      cmd = "SUGGEST #{collection_id} #{bucket_id} #{word_text}"
+      cmd += " LIMIT(#{limit})" unless limit.nil?
+      cmd += " OFFSET(#{limit})" unless offset.nil?
+      connection.connect do |peer|
+        peer.write(cmd)
+        message = peer.read(512) # PENDING CjPvE5t9
+        puts "Sent #{data}, got response: #{message}"
+      end
+      search_done = false
+      while search_done
+        Async do |_task|
+          endpoint.connect do |peer|
+            message = peer.read(512) # received EVENT SUGGEST CjPvE5t9
+            puts "Sent #{data}, got response: #{message}"
+            search_done = true
+          end
+        end
+      end
+    end
   end
 end
